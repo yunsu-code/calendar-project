2e77cfbc0a844854aecb4349ff2de40159dcd785
RByunsu <yss@rightbrain.co.kr>
2023-09-24 20:43:40 +0900

    캘린더 커스텀

--- src/view/home/Home.tsx
+++ src/view/home/Home.tsx
@@ -4,19 +4,40 @@ import DatePicker from "react-datepicker";
 import { getDate } from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
 import "@style/override/datepicker-custom.scss";
+import "@style/override/antd-custom.scss";
 import styles from "@style/home/home.module.scss";
 import useMediaQuery from "@assets/js/useMediaQuery";
 import cx from "classnames";
-
+import { PlusOutlined } from "@ant-design/icons";
+import { FloatButton, Modal, Input } from "antd";
+import type { SearchProps } from "../Search";
+const { Search } = Input;
 interface HomeProps {}
 
 const Home: FC<HomeProps> = ({}) => {
   const isTablet = useMediaQuery(900);
   const [startDate, setStartDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [modalText, setModalText] = useState("Content of the modal");
 
   const renderDay = (day: any, date: any) => {
     return <span className={styles.dayBtn}>{getDate(date)}</span>;
   };
+  console.log(startDate);
+
+  const showModal = () => {
+    setModalText(startDate.toLocaleDateString());
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setOpen(false);
+  };
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setOpen(false);
+  };
+  const onSearch: SearchProps["onSearch"] = (value: any, _e: any, info: any) =>
+    console.log(value);
 
   return (
     <div
@@ -31,6 +52,18 @@ const Home: FC<HomeProps> = ({}) => {
         onChange={(date: Date) => setStartDate(date)}
         renderDayContents={renderDay}
       />
+      <FloatButton
+        icon={<PlusOutlined style={{ color: "#fff" }} />}
+        onClick={showModal}
+      />
+      <Modal
+        title={modalText}
+        open={open}
+        onCancel={handleCancel}
+        onOk={handleOk}
+      >
+        <Search placeholder="할 일을 입력하세요." onSearch={onSearch} />
+      </Modal>
     </div>
   );
 };
